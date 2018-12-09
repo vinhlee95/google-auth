26ec7b73bcc107017907ce72251bca86e1dd6920
vinhle <lethanhvinh95@gmail.com>
2018-12-09 23:25:11 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import keys from './config/keys';
+
+class GoogleAuth extends Component {
+	state = {
+		isSignedIn: false,
+	}
+
+	async componentDidMount() {
+		// load up google api lib
+		window.gapi.load('client: auth2', () => {
+			window.gapi.client.init({
+				clientId: keys.google_oauth_client_id,
+				scope: 'email',
+			}).then(() => {
+				this.auth = window.gapi.auth2.getAuthInstance();
+				this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+				// listen for sign-in state change
+				this.auth.isSignedIn.listen(this.updateSigninStatus);
+			});
+		});
+	}
+
+	updateSigninStatus = () => {
+		this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+	}
+
+	googleLogin = () => this.auth.signIn();
+
+	googleLogout = () => this.auth.signOut();
+
+  render() {
+		const { isSignedIn } = this.state; console.log(isSignedIn);
+    return (
+      <div>
+				<button
+					onClick={isSignedIn ? this.googleLogout : this.googleLogin}
+					style={{
+						fontSize: 25,
+					}}
+				>
+					{
+						isSignedIn ?
+						'Log out' :
+						'Login with Google'
+					}
+				</button>
+      </div>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GoogleAuth />
       </div>
     );
   }
---
+++ src/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	google_oauth_client_id: "648506583905-g13ardmv4qo0tekt130iqg0855cmu0ci.apps.googleusercontent.com",
+};
